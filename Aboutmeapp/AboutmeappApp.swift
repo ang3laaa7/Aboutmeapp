7defe654c6983f431624573b24535cca786ba1ee
ang3laaa7 <angelachanc321@gmail.com>
2024-06-25 11:29:01 -0400

    Initial Commit

---
+++ Aboutmeapp/AboutmeappApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutmeappApp.swift
+//  Aboutmeapp
+//
+//  Created by Angela on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutmeappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
